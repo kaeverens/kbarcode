68230c852dfe6d11ce503975882c1c28a87edb53
Kae Verens <kae.verens@gmail.com>
2015-06-29 21:28:07 +0100

    add ITF format (12 digits)

--- kbarcode.js
+++ kbarcode.js
@@ -132,6 +132,7 @@ self.addEventListener('message', function(e) {
 				// if anything at all is wrong from this point forward, then we don't bother checking again at this bit size
 				// first, recalibrate the average gray value
 				calibrateGray(bcStart, rStart);
+				var barcodeType='';
 				// { check for start marker
 				if (!this_getBit(bcStart, bcBitSize) ||
 					this_getBit(bcStart+bcBitSize, bcBitSize) ||
@@ -153,124 +154,214 @@ self.addEventListener('message', function(e) {
 				result.rightMarker=[rGuardStart, rGuardStart+bcBitSize, rGuardStart+bcBitSize*2];
 				result.level=4;
 				// }
-				// { check middle marker
-				var mStart=parseInt(bcStart+bcWidth/2-bcBitSize*2.5);
-				if (this_getBit(mStart, bcBitSize) ||
-					!this_getBit(mStart+bcBitSize, bcBitSize) ||
-					this_getBit(mStart+bcBitSize*2, bcBitSize) ||
-					!this_getBit(mStart+bcBitSize*3, bcBitSize) ||
-					this_getBit(mStart+bcBitSize*4, bcBitSize)
-				) {
-					break;
-				}
-				result.middleMarker=[mStart, mStart+bcBitSize, mStart+bcBitSize*2, mStart+bcBitSize*3, mStart+bcBitSize*4];
-				result.level=5;
-				// }
-				// awesome! this is looking good. now we can extract the 7-bit bytes inside the barcode.
-				// adjust the guard boundaries so they're right at their edges
-				while (this_pixels[rGuardStart-1]<this_avgGray) {
-					rGuardStart--;
-				}
-				// both sides can be done the same way, so do it in a function
-				result.lBytesLBound=parseInt(bcStart+3*bcBitSize);
-				while (this_pixels[result.lBytesLBound]<this_avgGray) {
-					result.lBytesLBound++;
-				}
-				result.lBytesRBound=mStart-1;
-				result.rBytesLBound=mStart+5*bcBitSize;
-				while (this_pixels[result.rBytesLBound-1]<this_avgGray) {
-					result.rBytesLBound--;
-				}
-				result.rBytesRBound=rGuardStart-1;
-				while (this_pixels[result.rBytesRBound]<this_avgGray) {
-					result.rBytesRBound--;
-				}
-				var lbytes=getBytes(result.lBytesLBound, result.lBytesRBound),
-					rbytes=getBytes(result.rBytesLBound, result.rBytesRBound);
-				result.level=6;
-				result.lbytes=lbytes;
-				result.rbytes=rbytes;
-				// decode the bytes into digits
-				var digitSets=[
-					['0001101', '0100111', '1110010'],
-					['0011001', '0110011', '1100110'],
-					['0010011', '0011011', '1101100'],
-					['0111101', '0100001', '1000010'],
-					['0100011', '0011101', '1011100'],
-					['0110001', '0111001', '1001110'],
-					['0101111', '0000101', '1010000'],
-					['0111011', '0010001', '1000100'],
-					['0110111', '0001001', '1001000'],
-					['0001011', '0010111', '1110100']
-				];
-				var parityValues={
-					'AAAAAA': 0,
-					'AABABB': 1,
-					'AABBAB': 2,
-					'AABBBA': 3,
-					'ABAABB': 4,
-					'ABBAAB': 5,
-					'ABBBAA': 6,
-					'ABABAB': 7,
-					'ABABBA': 8,
-					'ABBABA': 9
-				};
-				var digits=[], ok=1, parityPattern='';
-				for (i=0;i<6;++i) { // check left side digits first
-					for (j=0;j<10;++j) {
-						if (digitSets[j][0]==lbytes[i]) {
-							digits[i]=j;
-							parityPattern+='A';
-							break;
+				if (!this_getBit(bcStart+bcBitSize*3, bcBitSize) && this_getBit(rGuardStart-bcBitSize, bcBitSize)) { // probably ITF-14
+					result.type='itf';
+					result.lBytesLBound=parseInt(bcStart+3*bcBitSize);
+					while (this_pixels[result.lBytesLBound]>this_avgGray) {
+						result.lBytesLBound++;
+					}
+					result.rBytesRBound=rGuardStart-1;
+					while (this_pixels[result.rBytesRBound]<this_avgGray) {
+						result.rBytesRBound--;
+					}
+					result.numbits=14*(2*3+3); // num digits (14) multiplied by (num wide bits (2) by width (2.5) plus num narrow bits)
+					var bits=[], tbitsize=(result.rBytesRBound-result.lBytesLBound)/result.numbits;
+					result.bitsize=tbitsize;
+					var lengths=[], lengthsAt=-1, expectedBit=0;
+					for (var bit=0;bit<result.numbits;++bit) {
+						var b=this_getBit(result.lBytesLBound+bit*tbitsize, tbitsize)?1:0;
+						bits.push(b);
+						if (b!=expectedBit) {
+							lengthsAt++;
+							lengths[lengthsAt]=1;
+							expectedBit=expectedBit?0:1;
 						}
-						if (digitSets[j][1]==lbytes[i]) {
-							digits[i]=j;
-							parityPattern+='B';
-							break;
+						else {
+							lengths[lengthsAt]++;
 						}
 					}
-					if (j==10) { // failed to identify a digit!
-						ok=0;
+					if (lengthsAt!=59) { // should be (12 digits * 5 bits -1 offset = 59)
+						continue;
 					}
-				}
-				var parityNumber=parityValues[parityPattern];
-//				return self.postMessage(result);
-				if (parityNumber===undefined) { // failed to find a number encoded in the parity bits
-					continue;
-				}
-				for (i=0;i<6;++i) { // check right side digits first
-					for (j=0;j<10;++j) {
-						if (digitSets[j][2]==rbytes[i]) {
-							digits[i+6]=j;
-							break;
+					result.bits=bits;
+					var bytes=[];
+					for (var i=0;i<6;++i) { // convert lengths into 5-bit bytes
+						for (var j=0;j<2;++j) {
+							var bits=[];
+							for (var k=0;k<5;++k) {
+								bits.push(lengths[i*10+j+k*2]);
+							}
+							bytes.push(bits);
 						}
 					}
-					if (j==10) { // failed to identify a digit!
-						ok=0;
+					for (var i=0;i<bytes.length;++i) { // "normalise" the bytes by zeroing the lowest 3 bits
+						for (var j=0;j<3;++j) {
+							var lowest=200, lowestIdx=0;
+							for (var k=0;k<5;++k) {
+								if (bytes[i][k] && bytes[i][k]<lowest) {
+									lowest=bytes[i][k];
+									lowestIdx=k;
+								}
+							}
+							bytes[i][lowestIdx]=0;
+						}
+						for (var j=0;j<5;++j) {
+							if (bytes[i][j]) {
+								bytes[i][j]=1;
+							}
+						}
+						// convert to "bitstring"
+						bytes[i]=bytes[i].join('');
 					}
+					// { convert the bytes to numbers
+					var itfBytes={
+						'00110':0,
+						'10001':1,
+						'01001':2,
+						'11000':3,
+						'00101':4,
+						'10100':5,
+						'01100':6,
+						'00011':7,
+						'10010':8,
+						'01010':9
+					};
+					var ok=true;
+					for (var i=0;i<12;++i) {
+						var n=itfBytes[bytes[i]];
+						if (n===undefined) {
+							ok=false;
+						}
+						else {
+							bytes[i]=n;
+						}
+					}
+					if (!ok) {
+						continue;
+					}
+					result.value=bytes.join('');
+					// }
+					return self.postMessage(result);
 				}
-				if (!ok) {
-					break;
+				else { // probably EAN-13
+					result.type='ean13';
+					// { check middle marker
+					var mStart=parseInt(bcStart+bcWidth/2-bcBitSize*2.5);
+					if (this_getBit(mStart, bcBitSize) ||
+						!this_getBit(mStart+bcBitSize, bcBitSize) ||
+						this_getBit(mStart+bcBitSize*2, bcBitSize) ||
+						!this_getBit(mStart+bcBitSize*3, bcBitSize) ||
+						this_getBit(mStart+bcBitSize*4, bcBitSize)
+					) {
+						break;
+					}
+					result.middleMarker=[mStart, mStart+bcBitSize, mStart+bcBitSize*2, mStart+bcBitSize*3, mStart+bcBitSize*4];
+					result.level=5;
+					// }
+					// awesome! this is looking good. now we can extract the 7-bit bytes inside the barcode.
+					// adjust the guard boundaries so they're right at their edges
+					while (this_pixels[rGuardStart-1]<this_avgGray) {
+						rGuardStart--;
+					}
+					// both sides can be done the same way, so do it in a function
+					result.lBytesLBound=parseInt(bcStart+3*bcBitSize);
+					while (this_pixels[result.lBytesLBound]<this_avgGray) {
+						result.lBytesLBound++;
+					}
+					result.lBytesRBound=mStart-1;
+					result.rBytesLBound=mStart+5*bcBitSize;
+					while (this_pixels[result.rBytesLBound-1]<this_avgGray) {
+						result.rBytesLBound--;
+					}
+					result.rBytesRBound=rGuardStart-1;
+					while (this_pixels[result.rBytesRBound]<this_avgGray) {
+						result.rBytesRBound--;
+					}
+					var lbytes=getBytes(result.lBytesLBound, result.lBytesRBound),
+						rbytes=getBytes(result.rBytesLBound, result.rBytesRBound);
+					result.level=6;
+					result.lbytes=lbytes;
+					result.rbytes=rbytes;
+					// decode the bytes into digits
+					var eanDigits=[
+						['0001101', '0100111', '1110010'],
+						['0011001', '0110011', '1100110'],
+						['0010011', '0011011', '1101100'],
+						['0111101', '0100001', '1000010'],
+						['0100011', '0011101', '1011100'],
+						['0110001', '0111001', '1001110'],
+						['0101111', '0000101', '1010000'],
+						['0111011', '0010001', '1000100'],
+						['0110111', '0001001', '1001000'],
+						['0001011', '0010111', '1110100']
+					];
+					var parityValues={
+						'AAAAAA': 0,
+						'AABABB': 1,
+						'AABBAB': 2,
+						'AABBBA': 3,
+						'ABAABB': 4,
+						'ABBAAB': 5,
+						'ABBBAA': 6,
+						'ABABAB': 7,
+						'ABABBA': 8,
+						'ABBABA': 9
+					};
+					var digits=[], ok=1, parityPattern='';
+					for (i=0;i<6;++i) { // check left side digits first
+						for (j=0;j<10;++j) {
+							if (eanDigits[j][0]==lbytes[i]) {
+								digits[i]=j;
+								parityPattern+='A';
+								break;
+							}
+							if (eanDigits[j][1]==lbytes[i]) {
+								digits[i]=j;
+								parityPattern+='B';
+								break;
+							}
+						}
+						if (j==10) { // failed to identify a digit!
+							ok=0;
+						}
+					}
+					var parityNumber=parityValues[parityPattern];
+					if (parityNumber===undefined) { // failed to find a number encoded in the parity bits
+						continue;
+					}
+					for (i=0;i<6;++i) { // check right side digits first
+						for (j=0;j<10;++j) {
+							if (eanDigits[j][2]==rbytes[i]) {
+								digits[i+6]=j;
+								break;
+							}
+						}
+						if (j==10) { // failed to identify a digit!
+							ok=0;
+						}
+					}
+					if (!ok) {
+						break;
+					}
+					digits.unshift(parityNumber);
+					// check to see if the checksum works against the digits
+					var odd=0, even=0;
+					for (i=12; i; i-=2) {
+						odd+=digits[i];
+					}
+					for (i=11; i>0; i-=2) {
+						even+=digits[i];
+					}
+					sum=odd+even*3;
+					sum=10-(sum%10);
+					if (digits[0]!=sum) { // checksum failed
+						break;
+					}
+					result.digits=digits;
+					result.success=1;
+					result.value=result.digits.join('');
+					return self.postMessage(result);
 				}
-				digits.unshift(parityNumber);
-				// check to see if the checksum works against the digits
-				var odd=0, even=0;
-				for (i=12; i; i-=2) {
-					odd+=digits[i];
-				}
-				for (i=11; i>0; i-=2) {
-					even+=digits[i];
-				}
-				sum=odd+even*3;
-				sum=10-(sum%10);
-				if (digits[0]!=sum) { // checksum failed
-					break;
-				}
-
-				result.digits=digits;
-				result.success=1;
-				result.value=result.digits.join('');
-				return self.postMessage(result);
 				// }
 			}
 			// }
